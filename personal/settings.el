bd089b390b35acc4793892574abbdc2677595872
Marcin Radoszewski <moriturius@gmail.com>
2015-12-12 10:41:22 +0000

    Added personal settings

---
+++ personal/settings.el
@@ -0,0 +1,10 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(require 'powerline)
+(powerline-default-theme)
+(set-frame-font "Iosevka 13")
+
+(provide 'settings)
+;;; settings.el ends here
