a38446e42cdb854c4ed8c016de8ad3a787a91dce
Marcin Radoszewski <moriturius@gmail.com>
2016-02-24 19:04:58 +0000

    Updated settings

--- personal/settings.el
+++ personal/settings.el
@@ -6,5 +6,10 @@
 (powerline-default-theme)
 (set-frame-font "Iosevka 13")
 
+(setq racer-cmd "/usr/bin/racer")
+(setq racer-rust-src-path "/home/morti/apps/rust/src")
+
+(scroll-bar-mode -1)
+
 (provide 'settings)
 ;;; settings.el ends here
